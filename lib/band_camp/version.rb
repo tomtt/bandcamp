f0fb125920de71aae6c2a236876a29d31b1456ec
Tom ten Thij <git@tomtenthij.nl>
2010-09-10 20:23:05 +0100

    Version 0.0.4

--- lib/band_camp/version.rb
+++ lib/band_camp/version.rb
@@ -1,4 +1,4 @@
 module BandCamp
   # We're doing this because bundler did it this way
-  VERSION = "0.0.3" unless defined?(::BandCamp::VERSION)
+  VERSION = "0.0.4" unless defined?(::BandCamp::VERSION)
 end
